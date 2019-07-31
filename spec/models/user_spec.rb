733fb800fd8408911b5eca598017b36111960931
Chris Lay <chrisklay93@gmail.com>
2019-07-31 13:32:21 -0400

    Add User Login/Sign-up Function

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:user) {User.new(first_name: 'Jim', last_name: 'Murray', username: 'WhiskyBoss', email:'WhiskyBoss@example.com', password: 'Whisky'  )}
+
+  describe "first_name" do
+    it "should return the user's first name" do
+      expect(user.first_name).to eq("Jim")
+    end
+  end
+
+  describe "last_name" do
+    it "should return the user's last name" do
+      expect(user.last_name).to eq("Murray")
+    end
+  end
+
+  describe "username" do
+    it "should return the user's username" do
+      expect(user.username).to eq("WhiskyBoss")
+    end
+  end
+
+  describe "email" do
+    it "should return the user's email" do
+      expect(user.email).to eq("WhiskyBoss@example.com")
+    end
+  end
+
+  describe "password" do
+    it "should return the user's password" do
+      expect(user.password).to eq("Whisky")
+    end
+  end
 end
