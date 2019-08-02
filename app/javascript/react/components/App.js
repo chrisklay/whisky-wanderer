b25742da85b0a85ba1247c6f7b1f5aa350085691
Chris Lay <chrisklay93@gmail.com>
2019-08-01 23:30:10 -0400

    Added Show Page and Containers

--- app/javascript/react/components/App.js
+++ app/javascript/react/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Route, Switch } from "react-router-dom"
 import RegionsIndexContainer from '../containers/RegionsIndexContainer'
+import RegionShowContainer from '../containers/RegionShowContainer'
 
 export const App = (props) => {
   return (
